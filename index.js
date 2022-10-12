5b7199ecbe90f2417cdc685b28a0590b76785e17
christian-kesler-2022 <111981585+christian-kesler-2022@users.noreply.github.com>
2022-10-12 11:37:23 -0700

    Create architecture

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-// run `node index.js` in the terminal
-
-console.log(`Hello Node.js v${process.versions.node}!`);
+var add_routes = require('./utils/add_routes.js');
+var docx_to_html = require('./utils/docx_to_html.js');
+var html_to_json = require('./utils/html_to_json.js');
+var pull_files = require('./utils/pull_files.js');
+var push_to_confluence = require('./utils/push_to_confluence.js');
