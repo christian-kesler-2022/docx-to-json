17897a5362222d336c3696cd06c540ad87a625b6
christian-kesler-2022 <111981585+christian-kesler-2022@users.noreply.github.com>
2022-10-12 11:57:13 -0700

    Expand on design

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-var add_routes = require('./utils/add_routes.js');
-var docx_to_html = require('./utils/docx_to_html.js');
-var html_to_json = require('./utils/html_to_json.js');
-var pull_files = require('./utils/pull_files.js');
-var push_to_confluence = require('./utils/push_to_confluence.js');
+var add_routes = require('./controllers/utils/add_routes.js');
+var docx_to_html = require('./controllers/utils/docx_to_html.js');
+var html_to_json = require('./controllers/utils/html_to_json.js');
+var pull_files = require('./controllers/utils/pull_files.js');
+var push_to_confluence = require('./controllers/utils/push_to_confluence.js');
+
+var express = require('express');
+var app = express();
+
+// var routes = require('./controllers/routes.js');
+// routes(app);
+
+var mammoth = require('mammoth');
+mammoth
+  .convertToHtml({ path: __dirname + '/model/input/sample.docx' })
+  .then(function (result) {
+    var html = result.value; // The generated HTML
+    console.log(result.value);
+    console.log(
+      '================================================================'
+    );
+    var messages = result.messages; // Any messages, such as warnings during conversion
+    console.log(result.value);
+  })
+  .done();
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.listen(3000);
